d5818d0ece957b689e83d6a71995f94dc9d7c248
620710306 Chonlada Srisook <119958890+ChonladaSr@users.noreply.github.com>
2024-09-21 19:38:59 +0700

    a

--- server.js
+++ server.js
@@ -40,6 +40,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+app.use('/uploads', express.static('uploads'));
+
+
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
